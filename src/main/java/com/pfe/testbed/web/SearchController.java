d3ab1cc721caa3c3bcc4ab815449489b5b3be69a
OpenAI QA Testbed Generator <qa-testbed@local.invalid>
2026-08-22 00:48:24 +0200

    qa: fix wrong HtmlUtils import in SearchController (unintended baseline defect, QA fixture maintenance)

--- src/main/java/com/pfe/testbed/web/SearchController.java
+++ src/main/java/com/pfe/testbed/web/SearchController.java
@@ -1,7 +1,7 @@
 package com.pfe.testbed.web;
 
 import org.springframework.http.MediaType;
-import org.springframework.util.HtmlUtils;
+import org.springframework.web.util.HtmlUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
